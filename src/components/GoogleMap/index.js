13663ae9cd23e517ed849aa263b252a862747970
BigBrosh <maksuhin@gmail.com>
2019-03-22 11:04:50 +0200

    - added custom cluster Icon.

--- src/components/GoogleMap/index.js
+++ src/components/GoogleMap/index.js
@@ -2,5 +2,5 @@ import GoogleMap from './GoogleMap';
 import { GoogleApiWrapper } from 'google-maps-react';
 
 export default GoogleApiWrapper({
-  apiKey: 'your api key'
+  apiKey: 'AIzaSyBGv0KVuJJgvgkDIz3EXM1Td83ZFmHw3xY'
 })(GoogleMap);
