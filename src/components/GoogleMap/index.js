3905e58c3de6e222390492f3232300afc6351a1e
BigBrosh <maksuhin@gmail.com>
2019-03-22 09:44:50 +0200

    fix.

--- src/components/GoogleMap/index.js
+++ src/components/GoogleMap/index.js
@@ -2,5 +2,5 @@ import GoogleMap from './GoogleMap';
 import { GoogleApiWrapper } from 'google-maps-react';
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyBGv0KVuJJgvgkDIz3EXM1Td83ZFmHw3xY'
+  apiKey: 'your api key'
 })(GoogleMap);
