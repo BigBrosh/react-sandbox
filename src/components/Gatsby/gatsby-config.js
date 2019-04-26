18d5cf0a4ad4783bca7204598e1401d2095eceb2
BigBrosh <maksuhin@gmail.com>
2019-04-26 07:01:50 +0300

    - implemented gatsby-contentful with @contentful/rich-text-react-renderer.

--- src/components/Gatsby/gatsby-config.js
+++ src/components/Gatsby/gatsby-config.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config();
+}
+
 module.exports = {
   plugins: [
     {
@@ -12,6 +16,13 @@ module.exports = {
       options: {
         pathToConfigModule: 'src/utils/typography'
       }
+    },
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
     },
     'gatsby-plugin-sass',
     'gatsby-plugin-styled-components'
