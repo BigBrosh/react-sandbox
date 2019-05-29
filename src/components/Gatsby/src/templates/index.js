add6905726b3296ece5a7f822c139cf9e03b437f
BigBrosh <maksuhin@gmail.com>
2019-05-29 07:03:25 +0300

    - added gatsby-plugin-react-helmet and react-helmet; - added helmet to main template with title and meta description.

--- src/components/Gatsby/src/templates/index.js
+++ src/components/Gatsby/src/templates/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import Helmet from 'react-helmet/es/Helmet';
 
 export default ({
   data: {
@@ -22,6 +23,11 @@ export default ({
 }) => {
   return (
     <Layout>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{name}</title>
+        <meta name="description" content={`${name} has wrote ${writtenBooks}`}/>
+      </Helmet>
       <h2>Author {name}</h2>
       <p>Wrote {writtenBooks}</p>
       {documentToReactComponents(JSON.parse(biography))}
